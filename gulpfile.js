c6decf91626a5edb9bbf7c5f6bfd1a4bdc95ba7e
NedrNout <maxim.nedozrelov@ya.ru>
2018-09-22 00:13:15 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -15,17 +15,31 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('sass/'))
         .pipe(browserSync.reload({stream: true}));
 });
-gulp.task('browserSync', function () {
+gulp.task('browserSync', ['start'], function () {
     browserSync({
         server: {
-            baseDir: 'sass',
+            baseDir: '../kursovik/',
         },
-        port: 9000
+        port: 3000
 
     });
 
 });
-gulp.task('watch', ['browserSync', 'start'], function () {
+gulp.task('watch', ['browserSync'], function () {
     gulp.watch('sass/*.sass', ['sass']);
     gulp.watch('*.html', browserSync.reload);
 });
+
+/*
+gulp.task('build', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlMinifier({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist/'));
+  return gulp.src('src/js/*.js')
+    .pipe(jsConcat('scripts.js'))
+    .pipe(jsMinification())
+    .pipe(gulp.dest('./dist/js/'));
+  return gulp.src('src/css/*.css')
+    .pipe(cssMinify({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/css/'));
+});*/
