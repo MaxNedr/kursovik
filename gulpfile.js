322cead550eb25d4ae49b71f62bcade32d30bb7b
Max Nedr <maxim.nedozrelov@ya.ru>
2018-09-21 15:38:36 +0300

    Карусель на странице single_page.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,31 @@
 var gulp = require('gulp');
-gulp.task('hello', function () {
-    console.log('hi dude')
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var jsonServer = require("gulp-json-srv");
+var server = jsonServer.create();
+
+gulp.task("start", function(){
+    return gulp.src("db.json")
+        .pipe(server.pipe());
+});
+
+gulp.task('sass', function () {
+    return gulp.src('sass/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('sass/'))
+        .pipe(browserSync.reload({stream: true}));
+});
+gulp.task('browserSync', function () {
+    browserSync({
+        server: {
+            baseDir: 'sass',
+        },
+        port: 9000
+
+    });
+
+});
+gulp.task('watch', ['browserSync', 'start'], function () {
+    gulp.watch('sass/*.sass', ['sass']);
+    gulp.watch('*.html', browserSync.reload);
 });
