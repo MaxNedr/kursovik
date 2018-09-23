8c7df98ff8c37d5a2e9f5981062f61f3eb785ba8
NedrNout <maxim.nedozrelov@ya.ru>
2018-09-23 20:27:04 +0300

    gulp

--- src/guulpss.js
+++ src/guulpss.js
@@ -5,11 +5,8 @@ var jsonServer = require("gulp-json-srv");
 var server = jsonServer.create();
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
-var useref = require('gulp-useref');
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
-    var minifyCss = require('gulp-clean-css');
-
+var minifyjs = require('gulp-js-minify');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task("start", function () {
     return gulp.src("src/db.json")
@@ -38,28 +35,16 @@ gulp.task('watch', ['browserSync'], function () {
 });
 
 
-
 gulp.task('build', function () {
-    return gulp.src('src/*.html')
-        .pipe(useref())
-        .pipe(gulpif('src/*.js', uglify()))
-        .pipe(gulpif('src/*.css', minifyCss()))
-        .pipe(gulp.dest('src/dist'));
-});
-
-
-/*gulp.task('build', function () {
     return gulp.src('src/sass/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('src/dist'))
-
-
-
-});*/
-/*.pipe(gulp.src('src/*.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gulp.src('src/*.js'))
         .pipe(minifyjs())
-        .pipe(gulp.dest('src/dist'))*/
-/*.pipe(gulp.src('src/*.html'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gulp.src('src/*.html'))
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('src/dist'));*/
+        .pipe(gulp.dest('dist'));
+
+});
